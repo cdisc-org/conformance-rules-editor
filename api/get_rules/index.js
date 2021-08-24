3d5162c03c9702e12e3277b2822b92ced9aefc6f
Nicholas Haydel <nhaydel@cdisc.org>
2021-08-24 14:03:30 -0500

    Add get request for all rules

--- api/get_rules/index.js
+++ api/get_rules/index.js
@@ -1,5 +1,35 @@
+const https = require('https');
+
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
+    const url = process.env["API_BASE_URL"]
+    const options = {
+        hostname: url,
+        path: '/jsonapi/node/rule',
+        method: 'GET',
+        headers: {
+            
+        }
+    }
+
+    var resp_body = ""
+    await new Promise((resolve, reject) => {
+        https.get(options, (resp) => {
+            resp.on("data", (chunk) => {
+                resp_body += chunk
+            });
+
+            resp.on("end", () => {
+                context.res.json({
+                    body: resp_body
+                })
+                context.done();
+            })
+        }).on('error', (error) => {
+            context.res = {
+                status: 500,
+                body: error
+            };
+            context.done();
+        }).end()
+    })
+}

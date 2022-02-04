37d4b1483c735997b8ab99ea8236e240b7ec88e8
Geraud Campion <gcampion@cdisc.org>
2022-02-04 14:18:11 -0500

    Get pseudo domain name from dataset name when test data has supp or relational datasets

--- src/utils/ExcelDataset.ts
+++ src/utils/ExcelDataset.ts
@@ -101,6 +101,14 @@ const getRecords = (cols: string[], rows: {}[]): {} => {
   );
 };
 
+const getDomainName = (rows: {}[], sheetName: string): string => {
+  return rows.length > 3 && rows[3]["DOMAIN"]
+    ? rows[3]["DOMAIN"]
+    : /* This is technically not the domain name, 
+    but it is what the CORE engine currently expects */
+      sheetName.toUpperCase().replace(".XPT", "");
+};
+
 export const excelToJsonDatasets = async (file: File): Promise<IDataset[]> => {
   const workbook: WorkBook = read(await readFile(file), {
     type: "binary",
@@ -117,9 +125,7 @@ export const excelToJsonDatasets = async (file: File): Promise<IDataset[]> => {
     setDatatypes(sheet);
     const rows: {}[] = utils.sheet_to_json<{}>(sheet);
 
-    if (rows.length > 3 && rows[3]["DOMAIN"]) {
-      dataset.domain = rows[3]["DOMAIN"];
-    }
+    dataset.domain = getDomainName(rows, sheetName);
 
     const sheetJson: string[][] = utils.sheet_to_json<string[]>(sheet, {
       header: 1,

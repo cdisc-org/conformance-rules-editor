df22e00a16239dbb1c35c5392e87a270fbedbe96
gcampion-cdisc <85252124+gcampion-cdisc@users.noreply.github.com>
2022-05-13 13:57:53 -0400

    Reloading test data works (#34)

--- src/components/TestStep/LoadTestStep.tsx
+++ src/components/TestStep/LoadTestStep.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AppContext, { Status, Steps } from "../AppContext";
 import { excelToJsonDatasets, IDataset } from "../../utils/ExcelDataset";
 import TestStep from "./TestStep";
@@ -6,24 +6,27 @@ import FileInput from "../FileInput/FileInput";
 
 export default function LoadTestStep() {
   const { loadCheck, setLoadCheck, selectedRule } = useContext(AppContext);
+  const [file, setFile] = useState<File>();
 
-  const handleFileSelected = async (event) => {
-    if (event.target.files.length) {
-      const file: File = event.target.files[0];
+  useEffect(() => {
+    const loadExcel = async () => {
       const data: IDataset[] = await excelToJsonDatasets(file);
       setLoadCheck({
         status: Status.Pass,
         details: [file, data],
       });
+    };
+    if (file) {
+      loadExcel();
     }
-  };
+  }, [setLoadCheck, file]);
 
   return (
     <TestStep title="Load Test Data" step={Steps.Load} results={loadCheck}>
       <FileInput
         id={selectedRule}
         label="Test Datasets File..."
-        onChange={handleFileSelected}
+        setFile={setFile}
       />
     </TestStep>
   );

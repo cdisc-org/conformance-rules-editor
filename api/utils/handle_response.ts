b9ac5031216c27495368f154347ca18400b919d4
gerrycampion <85252124+gerrycampion@users.noreply.github.com>
2023-03-01 13:33:39 -0500

    163 exception logging in application insights (#164)
    * testing debug level function logging
    
    * error logging goes to insights instead of client

--- api/utils/handle_response.ts
+++ api/utils/handle_response.ts
@@ -1,10 +1,3 @@
 export default async (context, getResponseContent) => {
-  try {
-    context.res = await getResponseContent();
-  } catch (error) {
-    context.res = {
-      status: 500,
-      body: error,
-    };
-  }
+  context.res = await getResponseContent();
 };

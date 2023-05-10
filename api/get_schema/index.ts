a450e9ff5f1b3bd6c2e8d6e78780a064bcd4b1ba
Gerry Campion <gcampion@cdisc.org>
2023-05-10 11:17:56 -0400

    Rules schema url moved to env var

--- api/get_schema/index.ts
+++ api/get_schema/index.ts
@@ -3,14 +3,11 @@ import handle_response from "../utils/handle_response";
 
 export default async (context, req) => {
   const res = await (
-    await fetch(
-      "https://raw.githubusercontent.com/cdisc-org/cdisc-rules-engine/main/resources/schema/CORE-base.json",
-      {
-        headers: {
-          Accept: "application/json",
-        },
-      }
-    )
+    await fetch(process.env["RULE_SCHEMA_URL"], {
+      headers: {
+        Accept: "application/json",
+      },
+    })
   ).json();
   return await handle_response(context, async () => ({
     body: res,

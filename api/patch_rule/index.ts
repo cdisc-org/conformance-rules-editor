d54b219decf2c916f88613facdb590a7c2203200
gerrycampion <85252124+gerrycampion@users.noreply.github.com>
2023-08-22 15:14:37 -0400

    Don't allow core id to be set manually (#199)

--- api/patch_rule/index.ts
+++ api/patch_rule/index.ts
@@ -1,9 +1,14 @@
 import { STORAGE_PROVIDER } from "../providers/BaseStorage";
 import handle_response from "../utils/handle_response";
-import { formatYAML } from "../utils/json_yaml";
+import { formatYAML, removeInvalidCoreid } from "../utils/json_yaml";
 
 export default async (context, req) => {
-  req.body.content = formatYAML(req.body.content);
+  req.body.content = formatYAML(
+    removeInvalidCoreid(
+      req.body.content,
+      (await STORAGE_PROVIDER.getRule(context.bindingData.id)).content
+    )
+  );
   await handle_response(context, async () => ({
     body: await STORAGE_PROVIDER.patchRule(context.bindingData.id, req.body),
   }));

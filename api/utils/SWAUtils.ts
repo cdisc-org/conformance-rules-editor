fe3154b77c0736e7d155d5e4358bc9d392562731
Gerry Campion <gcampion@cdisc.org>
2023-09-06 09:26:48 -0400

    typing for azure function user

--- api/utils/SWAUtils.ts
+++ api/utils/SWAUtils.ts
@@ -1,6 +1,13 @@
 import { HttpRequest } from "@azure/functions";
 
-export const getUser = (request: HttpRequest) =>
+export const getUser = (
+  request: HttpRequest
+): {
+  identityProvider: string;
+  userId: string;
+  userDetails: string;
+  userRoles: string[];
+} =>
   JSON.parse(
     Buffer.from(request.headers["x-ms-client-principal"], "base64").toString(
       "ascii"

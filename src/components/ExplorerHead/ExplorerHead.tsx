4f4da6c265076117751a8698fb61f3d8c6619e7c
Samuel Johnson <sfjohnson24@gmail.com>
2025-02-06 21:03:06 -0500

    removed log

--- src/components/ExplorerHead/ExplorerHead.tsx
+++ src/components/ExplorerHead/ExplorerHead.tsx
@@ -124,7 +124,6 @@ export default function ExplorerHead() {
             
             return value?.toString() || '';
           } catch (error) {
-            console.error('Error accessing custom column path:', error);
             return '';
           }
         },

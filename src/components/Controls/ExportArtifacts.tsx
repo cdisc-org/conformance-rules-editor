e4c028c790b22efd39e302bf1a05dafb4072bef8
Aleksei Furmenkov <alexeyfurmenkov@gmail.com>
2025-11-21 10:15:43 +0100

    Update ExportArtifacts.tsx

--- src/components/Controls/ExportArtifacts.tsx
+++ src/components/Controls/ExportArtifacts.tsx
@@ -33,7 +33,7 @@ export default function ExportArtifacts({
           JSON.stringify(jsonCheck.details[0].details, null, 4)
         );
         zip.file("Define.xml", loadDefineXMLCheck.details[1]?.details ?? "");
-        if (loadDatasetsCheck.details[0]?.details) {
+        if (loadDatasetsCheck?.details?.[0]?.details) {
           zip.file("Datasets.xlsx", loadDatasetsCheck.details[0].details);
         }
         zip.file(
